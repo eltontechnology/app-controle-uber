dc404abd037a98fd294249ae8fc623648ebbac70
Elton Vieira <technology.elton@gmail.com>
2024-11-05 09:14:05 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let transacoes = [];
 let mesAtual = new Date().getMonth();
 let anoAtual = new Date().getFullYear();
+let transacaoEditandoIndex = null;
 
 document.addEventListener('DOMContentLoaded', () => {
     inicializarDatepicker();
@@ -8,10 +9,9 @@ document.addEventListener('DOMContentLoaded', () => {
     atualizarTituloMes();
     exibirTransacoes();
 
-    document.getElementById('adicionar').addEventListener('click', adicionarTransacao);
+    document.getElementById('adicionar').addEventListener('click', adicionarOuAtualizarTransacao);
     document.getElementById('mes-anterior').addEventListener('click', () => mudarMes(-1));
     document.getElementById('mes-proximo').addEventListener('click', () => mudarMes(1));
-    document.getElementById('exportar-csv').addEventListener('click', exportarParaCSV);
 });
 
 function inicializarDatepicker() {
@@ -35,18 +35,17 @@ function atualizarTituloMes() {
     document.getElementById('mes-atual').textContent = `${obterNomeMes(mesAtual)} ${anoAtual}`;
 }
 
-function adicionarTransacao() {
+function adicionarOuAtualizarTransacao() {
     const dataTransacao = document.getElementById('data').value;
     const descricao = document.getElementById('descricao').value.trim();
     const valor = parseFloat(document.getElementById('valor').value);
-    const tipo = document.querySelector('input[name="tipo"]:checked').value; // Obtém o valor do rádio selecionado
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
     if (!dataTransacao || !descricao || isNaN(valor) || valor <= 0) {
         alert('Por favor, preencha todos os campos corretamente.');
         return;
     }
 
-    // Converter a data do formato DD/MM/YYYY para YYYY-MM-DD para criar o objeto Date corretamente
     const partesData = dataTransacao.split('/');
     const dataFormatada = new Date(partesData[2], partesData[1] - 1, partesData[0]);
 
@@ -59,7 +58,13 @@ function adicionarTransacao() {
         ano: dataFormatada.getFullYear()
     };
 
-    transacoes.push(transacao);
+    if (transacaoEditandoIndex !== null) {
+        transacoes[transacaoEditandoIndex] = transacao;
+        transacaoEditandoIndex = null;
+    } else {
+        transacoes.push(transacao);
+    }
+
     salvarTransacoes();
     exibirTransacoes();
     limparCampos();
@@ -80,12 +85,13 @@ function mudarMes(direcao) {
 
 function exibirTransacoes() {
     const historicosMeses = document.getElementById('historicos-meses');
-    historicosMeses.innerHTML = ''; // Limpa o conteúdo anterior
+    historicosMeses.innerHTML = '';
 
     const transacoesFiltradas = transacoes.filter(transacao => transacao.mes === mesAtual && transacao.ano === anoAtual);
 
     if (transacoesFiltradas.length === 0) {
         historicosMeses.innerHTML = `<p class="text-light">Nenhuma transação encontrada para ${obterNomeMes(mesAtual)} ${anoAtual}.</p>`;
+        atualizarTotais([]);
         return;
     }
 
@@ -96,29 +102,28 @@ function exibirTransacoes() {
 
 function criarTabela(transacoesFiltradas) {
     const tabela = document.createElement('div');
-    tabela.classList.add('table-responsive'); // Adiciona classe de responsividade do Bootstrap
-    tabela.classList.add('table-container');
+    tabela.classList.add('table-responsive', 'table-container');
     tabela.innerHTML = `
         <table class="table table-dark table-hover">
             <thead>
                 <tr>
-                    <th>Data</th>
-                    <th>Descrição</th>
-                    <th>Valor</th>
-                    <th>Tipo</th>
-                    <th>Ações</th>
+                    <th>📅 Data</th>
+                    <th>📝 Descrição</th>
+                    <th>💰 Valor</th>
+                    <th>💱 Tipo</th>
+                    <th>👉 Ações</th>
                 </tr>
             </thead>
             <tbody>
                 ${transacoesFiltradas.map((transacao, index) => `
-                    <tr class="${transacao.tipo === 'entrada' ? 'historico-entrada' : 'historico-saida'}">
-                        <td data-label="Data">${transacao.data}</td>
-                        <td data-label="Descrição">${transacao.descricao}</td>
-                        <td data-label="Valor">R$ ${transacao.valor.toFixed(2)}</td>
-                        <td data-label="Tipo">${transacao.tipo.charAt(0).toUpperCase() + transacao.tipo.slice(1)}</td>
-                        <td data-label="Ações">
-                            <button class="btn btn-warning btn-sm mr-2" onclick="editarTransacao(${index})">✏️Editar</button>
-                            <button class="btn btn-danger btn-sm" onclick="confirmarExcluirTransacao(${index})">🗑️Excluir</button>
+                    <tr>
+                        <td>${transacao.data}</td>
+                        <td>${transacao.descricao}</td>
+                        <td>${transacao.tipo === 'saida' ? '-' : ''}R$ ${transacao.valor.toFixed(2)}</td>
+                        <td>${transacao.tipo.charAt(0).toUpperCase() + transacao.tipo.slice(1)}</td>
+                        <td>
+                            <button class="btn btn-sm btn-warning" onclick="editarTransacao(${index})">✏️ Editar</button>
+                            <button class="btn btn-sm btn-danger" onclick="excluirTransacao(${index})">🗑️ Excluir</button>
                         </td>
                     </tr>
                 `).join('')}
@@ -130,23 +135,11 @@ function criarTabela(transacoesFiltradas) {
 
 function editarTransacao(index) {
     const transacao = transacoes[index];
-    const dataFormatada = new Date(transacao.data.split('/').reverse().join('/')); // Converter para YYYY-MM-DD
-
-    // Preencher os campos do formulário com os dados da transação
-    $('.datepicker').datepicker('update', dataFormatada);
+    document.getElementById('data').value = transacao.data;
     document.getElementById('descricao').value = transacao.descricao;
     document.getElementById('valor').value = transacao.valor;
     document.querySelector(`input[name="tipo"][value="${transacao.tipo}"]`).checked = true;
-
-    // Remove a transação atual antes de editar
-    excluirTransacao(index);
-}
-
-function confirmarExcluirTransacao(index) {
-    const confirmar = confirm('Você tem certeza que deseja excluir esta transação?');
-    if (confirmar) {
-        excluirTransacao(index);
-    }
+    transacaoEditandoIndex = index;
 }
 
 function excluirTransacao(index) {
@@ -156,35 +149,25 @@ function excluirTransacao(index) {
 }
 
 function atualizarTotais(transacoesFiltradas) {
-    const totalEntradas = transacoesFiltradas
-        .filter(transacao => transacao.tipo === 'entrada')
-        .reduce((acc, transacao) => acc + transacao.valor, 0);
-    const totalSaidas = transacoesFiltradas
-        .filter(transacao => transacao.tipo === 'saida')
-        .reduce((acc, transacao) => acc + transacao.valor, 0);
-    const saldo = totalEntradas - totalSaidas;
+    const totalEntrada = transacoesFiltradas
+        .filter(t => t.tipo === 'entrada')
+        .reduce((acc, t) => acc + t.valor, 0);
+    const totalSaida = transacoesFiltradas
+        .filter(t => t.tipo === 'saida')
+        .reduce((acc, t) => acc + t.valor, 0);
+    const saldo = totalEntrada - totalSaida;
 
-    const totaisDiv = document.createElement('div');
-    totaisDiv.classList.add('totais');
-    totaisDiv.innerHTML = `
-        <h3>📈 Entradas: <span class="valor total-entrada">R$ ${totalEntradas.toFixed(2)}</span></h3>
-        <h3>📉 Saídas: <span class="valor total-saida">R$ ${totalSaidas.toFixed(2)}</span></h3>
-        <h3>📊 Saldo: <span class="valor saldo" style="color: ${saldo >= 0 ? '#00FFFF'  : 'red'};">R$ ${saldo.toFixed(2)}</span></h3>
-    `;
-
-    const container = document.querySelector('.table-container');
-    container.appendChild(totaisDiv);
-}
-
-function formatarValor(valor) {
-    return `R$ ${valor.toFixed(2)}`;
+    document.querySelector('.total-entrada').textContent = `R$ ${totalEntrada.toFixed(2)}`;
+    document.querySelector('.total-saida').textContent = `R$ ${totalSaida.toFixed(2)}`;
+    document.querySelector('.saldo').textContent = `R$ ${saldo.toFixed(2)}`;
+    document.querySelector('.saldo').style.color = saldo >= 0 ? '#00FFFF' : '#FF0000';
 }
 
 function limparCampos() {
-    $('.datepicker').datepicker('update', '');
+    document.getElementById('data').value = '';
     document.getElementById('descricao').value = '';
     document.getElementById('valor').value = '';
-    document.getElementById('entrada').checked = true; // Resetando para 'Entrada'
+    document.getElementById('entrada').checked = true;
 }
 
 function salvarTransacoes() {
@@ -192,46 +175,8 @@ function salvarTransacoes() {
 }
 
 function carregarTransacoes() {
-    const transacoesSalvas = JSON.parse(localStorage.getItem('transacoes'));
+    const transacoesSalvas = localStorage.getItem('transacoes');
     if (transacoesSalvas) {
-        transacoes = transacoesSalvas;
+        transacoes = JSON.parse(transacoesSalvas);
     }
 }
-
-function exportarParaCSV() {
-    if (transacoes.length === 0) {
-        alert('Nenhuma transação para exportar.');
-        return;
-    }
-
-    const headers = ['Data', 'Descrição', 'Valor', 'Tipo', 'Mês', 'Ano'];
-    const rows = transacoes.map(transacao => [
-        transacao.data,
-        transacao.descricao,
-        transacao.valor.toFixed(2),
-        transacao.tipo,
-        obterNomeMes(transacao.mes),
-        transacao.ano
-    ]);
-
-    let csvContent = 'data:text/csv;charset=utf-8,' 
-        + headers.join(',') + '\n'
-        + rows.map(e => e.join(',')).join('\n');
-
-    const encodedUri = encodeURI(csvContent);
-    const link = document.createElement('a');
-    link.setAttribute('href', encodedUri);
-    link.setAttribute('download', 'transacoes.csv');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
-
-document.addEventListener("keypress", function(e){
-    
-    if(e.key === "Enter"){
-        const btn = document.querySelector("#adicionar");
-        btn.click(); 
-        location.reload();
-    }
-});
